7e41f523457ed1262920c7390b939cda527930c1
vbtang <vbtang@vip.qq.com>
2017-03-15 00:05:31 +0800

    add dialog for SensorChoose and AcquirationParameters, add classes for these dialog

---
+++ Src/Edislab/Edislab Pro/DlgAcquirationPara.cpp
@@ -0,0 +1,34 @@
+// DlgAcquirationPara.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "Edislab Pro.h"
+#include "DlgAcquirationPara.h"
+#include "afxdialogex.h"
+
+
+// CDlgAcquirationPara 对话框
+
+IMPLEMENT_DYNAMIC(CDlgAcquirationPara, CBaseDialog)
+
+CDlgAcquirationPara::CDlgAcquirationPara(CWnd* pParent /*=NULL*/)
+	: CBaseDialog(CDlgAcquirationPara::IDD, pParent)
+{
+
+}
+
+CDlgAcquirationPara::~CDlgAcquirationPara()
+{
+}
+
+void CDlgAcquirationPara::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgAcquirationPara, CDialog)
+END_MESSAGE_MAP()
+
+
+// CDlgAcquirationPara 消息处理程序
